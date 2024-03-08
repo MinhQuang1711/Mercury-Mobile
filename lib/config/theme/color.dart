bf5ac63e3bbae8358db70ee662b9a62b3a8bcbad
Quang Phạm <phamminhquang.work@gmail.com>
2024-03-08 15:47:14 +0700

    Theme

---
+++ lib/config/theme/color.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static Color background = Colors.grey.shade200;
+
+  static const Color brown = Color(0XFFC2572D);
+  static const Color purple = Color(0XFF6509B3);
+  static const Color primary = Color(0XFF192BC2);
+
+  static const Color red = Color(0XFFEA2D1F);
+  static const Color redLight = Color(0XFFE46E63);
+
+  static const Color blue = Color(0XFF005EB3);
+  static const Color blueLight = Color(0XFF56CCF2);
+
+  static const Color green = Color(0XFF0D9D35);
+  static const Color greenLight = Color(0XFF45E381);
+
+  static const Color orange = Color(0XFFF46B45);
+  static const Color orangeLight = Color(0XFFEEA849);
+}
