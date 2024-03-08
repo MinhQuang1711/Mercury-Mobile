cab491223ff1f6d16cf4ade2bb79d1d891cb2a8f
Quang Phạm <phamminhquang.work@gmail.com>
2024-03-08 16:47:55 +0700

    Color

--- lib/config/theme/color.dart
+++ lib/config/theme/color.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color white = Colors.white;
-  static const Color black = Colors.black;
-  static Color background = Colors.grey.shade200;
+  static const Color white = Color(0XFFFDFDFD);
+  static const Color black = Color(0XFF0C0C0C);
 
-  static const Color brown = Color(0XFFC2572D);
-  static const Color purple = Color(0XFF6509B3);
-  static const Color primary = Color(0XFF192BC2);
+  static const Color grey1 = Color(0XFFFAFAFA);
+  static const Color grey2 = Color(0XFFF9F9F9);
+  static const Color grey3 = Color(0XFFEDEDED);
+  static const Color grey4 = Color(0XFF717171);
+  static const Color grey5 = Color(0XFF505050);
 
-  static const Color red = Color(0XFFEA2D1F);
-  static const Color redLight = Color(0XFFE46E63);
+  static const Color green = Color(0XFF00966D);
+  static const Color greenLight = Color(0XFFBFFEEA);
 
-  static const Color blue = Color(0XFF005EB3);
-  static const Color blueLight = Color(0XFF56CCF2);
+  static const Color yellow = Color(0XFFDEB53A);
+  static const Color yellowLight = Color(0XFFFFF8E1);
+  static const Color red = Color(0XFFED2E2E);
+  static const Color redLight = Color(0XFFFCE8E8);
 
-  static const Color green = Color(0XFF0D9D35);
-  static const Color greenLight = Color(0XFF45E381);
+  static const Color blue = Color(0XFF246BFD);
+  static const Color blueShade2 = Color(0XFF024ADF);
+  static const Color blueShade3 = Color(0XFF0240BF);
 
-  static const Color orange = Color(0XFFF46B45);
-  static const Color orangeLight = Color(0XFFEEA849);
+  static const Color blueTint = Color(0XFFCEDEFF);
+  static const Color buleTint2 = Color(0XFFE7EFFF);
+  static const Color blueTint3 = Color(0XFFB6CEFE);
 }
