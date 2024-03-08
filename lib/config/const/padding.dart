9f5de35f0472dc2a53b63b7d9ff16e18747cce73
Quang Phạm <phamminhquang.work@gmail.com>
2024-03-08 16:55:10 +0700

    padding, radius

---
+++ lib/config/const/padding.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppPadding {
+  static const padding4 = EdgeInsets.all(4);
+  static const padding8 = EdgeInsets.all(8);
+  static const padding12 = EdgeInsets.all(12);
+  static const padding16 = EdgeInsets.all(16);
+}
---
+++ lib/config/const/radius.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppRadius {
+  static const radius4 = Radius.circular(4);
+  static const radius6 = Radius.circular(6);
+  static const radius8 = Radius.circular(8);
+  static const radius10 = Radius.circular(10);
+}
