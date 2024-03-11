67200f311bf518512cbcafb8b6ad6e1e023dba1e
Quang Phạm <phamminhquang.work@gmail.com>
2024-03-11 14:35:29 +0700

    Token storage

---
+++ lib/core/utils/storage/app_storage.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: unused_field
+
+import 'dart:convert';
+
+import 'package:mercury/core/utils/log.dart';
+import 'package:mercury/feature/data/model/token/token.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TokenStorage {
+  TokenStorage._();
+  static final instance = TokenStorage._();
+  final String _tokenKey = "Token";
+
+  void saveToken({
+    required String jsonString,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(_tokenKey, jsonString);
+  }
+
+  Future<String?> _getJsonToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_tokenKey);
+  }
+
+  Future<Token?> getToken() async {
+    var jsonToken = await _getJsonToken();
+    try {
+      return Token.fromJson(jsonDecode(jsonToken ?? ""));
+    } catch (e) {
+      logError(e);
+      return null;
+    }
+  }
+}
