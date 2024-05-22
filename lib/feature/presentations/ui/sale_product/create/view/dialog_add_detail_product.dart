2e74b5b0f72b78d0c0275f958c9e14207641c1b3
Quang Phạm <phamminhquang.work@gmail.com>
2024-05-22 14:24:41 +0700

    a

--- lib/feature/presentations/ui/sale_product/create/view/dialog_add_detail_product.dart
+++ lib/feature/presentations/ui/sale_product/create/view/dialog_add_detail_product.dart
@@ -28,8 +28,8 @@ class _DialogAddDetailProductState extends State<DialogAddDetailProduct> {
   }
 
   void _changedIngredientId(SearchFieldListItem<ComboBox>? val) {
-    _detailProduct =
-        _detailProduct.copyWith(id: val?.item?.id, name: val?.item?.name);
+    _detailProduct = _detailProduct.copyWith(
+        id: val?.item?.id, name: val?.item?.name, price: val?.item?.price);
   }
 
   void _onSubmit() {
