0e2b429992415e660384b474cf37d9ce0eaad4a5
Quang Phạm <phamminhquang.work@gmail.com>
2024-05-23 10:45:00 +0700

    Load more ingredient

--- lib/feature/presentations/ui/ingredient/get/widget/list_view.dart
+++ lib/feature/presentations/ui/ingredient/get/widget/list_view.dart
@@ -2,16 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mercury/core/utils/extension/contetxt.dart';
 import 'package:mercury/feature/data/model/ingredinent/ingredient.dart';
+import 'package:mercury/feature/presentations/bloc/ingredient/bloc/bloc.dart';
+import 'package:mercury/feature/presentations/bloc/ingredient/bloc/event/event.dart';
 import 'package:mercury/feature/presentations/ui/ingredient/detail/detail.dart';
 import 'package:mercury/feature/presentations/ui/ingredient/get/widget/card.dart';
+import 'package:mercury/feature/presentations/widget/listen_scroll_widget.dart';
 
 import '../../../../bloc/ingredient/cubit/get/cubit.dart';
 import '../../../../bloc/ingredient/cubit/get/state/state.dart';
 import '../../../../widget/list_view/list_view.dart';
 import '../view/bottom_sheet.dart';
 
-class IngredientList extends StatelessWidget {
+class IngredientList extends StatefulWidget {
   const IngredientList({super.key});
+
+  @override
+  State<IngredientList> createState() => _IngredientListState();
+}
+
+class _IngredientListState extends State<IngredientList> {
+  final _controller = ScrollController();
+  late final IngredientBloc _bloc;
+  @override
+  void initState() {
+    _bloc = context.read<IngredientBloc>();
+    super.initState();
+  }
+
+  void _loadMore() {
+    final cubit = context.read<GetIngredientCubit>();
+    int nextPage = cubit.state.searchByName.pageNumber + 1;
+    final dto = cubit.state.searchByName.copyWith(pageNumber: nextPage);
+    _bloc.add(IngredientEvent.get(searchByName: dto));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,15 +56,20 @@ class IngredientList extends StatelessWidget {
     return Expanded(
       child: BlocBuilder<GetIngredientCubit, GetIngredientState>(
         builder: (context, state) {
-          return AppListView(
-            items: state.list,
-            child: ListView.builder(
-              itemCount: state.list.length,
-              itemBuilder: (context, index) => IngredientCard(
-                ingredient: state.list[index],
-                onTapMoreButton: onTapMoreButton,
-                onTap: (val) => context.showDialogAndListen(
-                    child: DetailIngredientScreen(ingredient: val)),
+          return ListenScrollWidget(
+            handleScrollEndOfList: _loadMore,
+            controller: _controller,
+            child: AppListView(
+              items: state.list,
+              child: ListView.builder(
+                controller: _controller,
+                itemCount: state.list.length,
+                itemBuilder: (context, index) => IngredientCard(
+                  ingredient: state.list[index],
+                  onTapMoreButton: onTapMoreButton,
+                  onTap: (val) => context.showDialogAndListen(
+                      child: DetailIngredientScreen(ingredient: val)),
+                ),
               ),
             ),
           );
