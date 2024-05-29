35237495d8a3a9581f033878b35cd0dadae53341
Quang Phạm <phamminhquang.work@gmail.com>
2024-05-29 15:00:49 +0700

    Cubit

---
+++ lib/feature/presentations/bloc/sale_invoice/cubit/common_sale_invoice_cubit/cubit.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: unused_result
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mercury/feature/domain/model/combo_box/combo_box.dart';
+import 'package:mercury/feature/domain/model/sale_invoice_request/sale_invoice_request.dart';
+import 'package:mercury/feature/presentations/bloc/sale_invoice/cubit/common_sale_invoice_cubit/state/state.dart';
+
+class CommonSaleInvoiceCubit extends Cubit<CommonSaleInvoiceState> {
+  CommonSaleInvoiceCubit()
+      : super(const CommonSaleInvoiceState(SaleInvoiceRequest()));
+
+  void selectDelailSaleInvoice(ComboBox val) {
+    final oldList = List<ComboBox>.from(state.request.detailSaleInvoice ?? []);
+    emit(
+      state.copyWith(
+        request: state.request.copyWith(
+          detailSaleInvoice: oldList..add(val.copyWith(quantity: 1)),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/feature/presentations/bloc/sale_invoice/cubit/common_sale_invoice_cubit/state/state.dart
@@ -0,0 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:mercury/feature/domain/model/sale_invoice_request/sale_invoice_request.dart';
+
+part 'state.freezed.dart';
+
+@freezed
+class CommonSaleInvoiceState with _$CommonSaleInvoiceState {
+  const factory CommonSaleInvoiceState(SaleInvoiceRequest request) = _State;
+}
