58c4eba5c9926c72b9c0c63abcf5a1dc9f05fd10
Quang Phạm <phamminhquang.work@gmail.com>
2024-05-29 15:32:57 +0700

    a

--- lib/feature/presentations/ui/sale_invoice/widget/price_of_product.dart
+++ lib/feature/presentations/ui/sale_invoice/widget/price_of_product.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mercury/core/utils/extension/number.dart';
+import 'package:mercury/feature/presentations/bloc/sale_invoice/cubit/common_sale_invoice_cubit/cubit.dart';
+import 'package:mercury/feature/presentations/bloc/sale_invoice/cubit/common_sale_invoice_cubit/state/state.dart';
 import 'package:mercury/feature/presentations/ui/sale_invoice/widget/info.dart';
 
 class PriceOfProduct extends StatelessWidget {
@@ -8,9 +11,19 @@ class PriceOfProduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SaleInvoiceInfo(
-      title: "Tiền sản phẩm",
-      content: price.formatNumber(),
+    return BlocBuilder<CommonSaleInvoiceCubit, CommonSaleInvoiceState>(
+      buildWhen: (previous, current) =>
+          previous.request.detailSaleInvoice !=
+          current.request.detailSaleInvoice,
+      builder: (context, state) {
+        final list = state.request.detailSaleInvoice ?? [];
+        return SaleInvoiceInfo(
+            title: "Tiền sản phẩm",
+            content: list
+                .fold<double>(
+                    0.0, (p, c) => p + ((c.quantity ?? 1) * (c.price ?? 0)))
+                .formatNumber());
+      },
     );
   }
 }
