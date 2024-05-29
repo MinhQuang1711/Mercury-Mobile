b1ba811895b2d34b6b0ecdbb47a5f429c3bd284d
Quang Phạm <phamminhquang.work@gmail.com>
2024-05-29 15:40:16 +0700

    a

--- lib/feature/presentations/ui/sale_invoice/widget/product_field.dart
+++ lib/feature/presentations/ui/sale_invoice/widget/product_field.dart
@@ -3,35 +3,45 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mercury/feature/domain/model/combo_box/combo_box.dart';
 import 'package:mercury/feature/presentations/bloc/combo_box/cubit.dart';
 import 'package:mercury/feature/presentations/bloc/combo_box/state/state.dart';
+import 'package:mercury/feature/presentations/bloc/sale_invoice/cubit/common_sale_invoice_cubit/cubit.dart';
+import 'package:mercury/feature/presentations/bloc/sale_invoice/cubit/common_sale_invoice_cubit/state/state.dart';
 import 'package:mercury/feature/presentations/widget/column_input/column_input.dart';
 import 'package:mercury/feature/presentations/widget/search_field/search_field.dart';
 import 'package:mercury/feature/presentations/widget/search_field/search_item.dart';
 import 'package:searchfield/searchfield.dart';
 
 class ProductField extends StatelessWidget {
-  const ProductField({super.key, this.prosuctSelcted, this.onTap});
-  final List<ComboBox>? prosuctSelcted;
+  const ProductField({super.key, this.onTap});
   final Function(SearchFieldListItem<ComboBox>)? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ComboBoxCubit, ComboBoxState>(
-        buildWhen: (previous, current) => previous.products != current.products,
-        builder: (context, state) {
-          // Khong hien thi cac san pham da chon
-          var allProducts = List<ComboBox>.from(state.products);
-          allProducts.removeWhere((e) =>
-              (prosuctSelcted ?? []).any((selected) => e.id == selected.id));
-          return ColumnInput(
-            isRequied: true,
-            titleLabel: "Sản phẩm",
-            bottomWidget: AppSearchFiled(
-              onTap: onTap,
-              items: allProducts,
-              hint: "Chọn sản phẩm",
-              appItemFields: AppItemField.comboBox,
-            ),
-          );
-        });
+    return BlocBuilder<CommonSaleInvoiceCubit, CommonSaleInvoiceState>(
+      buildWhen: (previous, current) =>
+          previous.request.detailSaleInvoice !=
+          current.request.detailSaleInvoice,
+      builder: (context, commonState) {
+        return BlocBuilder<ComboBoxCubit, ComboBoxState>(
+            buildWhen: (previous, current) =>
+                previous.products != current.products,
+            builder: (context, state) {
+              // Khong hien thi cac san pham da chon
+              var allProducts = List<ComboBox>.from(state.products);
+              allProducts.removeWhere((e) =>
+                  (commonState.request.detailSaleInvoice ?? [])
+                      .any((selected) => e.id == selected.id));
+              return ColumnInput(
+                isRequied: true,
+                titleLabel: "Sản phẩm",
+                bottomWidget: AppSearchFiled(
+                  onTap: onTap,
+                  items: allProducts,
+                  hint: "Chọn sản phẩm",
+                  appItemFields: AppItemField.comboBox,
+                ),
+              );
+            });
+      },
+    );
   }
 }
