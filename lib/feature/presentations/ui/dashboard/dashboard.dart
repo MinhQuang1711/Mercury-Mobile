282228d6c53427f25070fb64ebf00f626391422a
Quang Phạm <phamminhquang.work@gmail.com>
2024-06-05 17:02:51 +0700

    navigate

--- lib/feature/presentations/ui/dashboard/dashboard.dart
+++ lib/feature/presentations/ui/dashboard/dashboard.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:mercury/config/const/box_shadow.dart';
-import 'package:mercury/config/const/gradient.dart';
 import 'package:mercury/config/const/padding.dart';
 import 'package:mercury/config/const/radius.dart';
 import 'package:mercury/config/theme/color.dart';
 import 'package:mercury/config/theme/text_style.dart';
 import 'package:mercury/feature/presentations/ui/dashboard/widget/chart.dart';
+import 'package:mercury/feature/presentations/ui/dashboard/widget/create_invoice_row.dart';
 import 'package:mercury/feature/presentations/ui/dashboard/widget/overview_bar.dart';
-import 'package:mercury/gen/assets.gen.dart';
 
 import 'widget/gradient_background.dart';
 import 'widget/white_background.dart';
@@ -53,29 +52,7 @@ class DashboardPage extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             const OverViewBar(),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Expanded(
-                  child: _createInvoiceButton(
-                    color: AppColor.green,
-                    title: "Thêm hóa đơn bán",
-                    assetGenImage: Assets.icon.invoices,
-                    gradient: AppGradient.getGreenGradient(),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: _createInvoiceButton(
-                    title: "Thêm hóa đơn nhập",
-                    color: AppColor.blueShade2,
-                    assetGenImage: Assets.icon.import,
-                    gradient: AppGradient.getBlueGradient(),
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 20),
+            const CreateInvoiceRow(),
             Container(
               decoration: BoxDecoration(
                 color: AppColor.white,
@@ -86,36 +63,6 @@ class DashboardPage extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-
-  Container _createInvoiceButton({
-    required String title,
-    required Gradient gradient,
-    required AssetGenImage assetGenImage,
-    Color? color,
-  }) {
-    return Container(
-      padding: AppPadding.padding14,
-      decoration: BoxDecoration(
-        gradient: gradient,
-        borderRadius: AppContainerBorder.radius6,
-      ),
-      child: Column(
-        children: [
-          Text(
-            title,
-            style: bodyMedium.copyWith(color: color ?? AppColor.white),
-          ),
-          const SizedBox(height: 7),
-          Image.asset(
-            assetGenImage.keyName,
-            height: 25,
-            width: 25,
-            color: color ?? AppColor.white,
-          ),
-        ],
       ),
     );
   }
---
+++ lib/feature/presentations/ui/dashboard/widget/create_invoice_row.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mercury/config/router/path.dart';
+
+import '../../../../../config/const/gradient.dart';
+import '../../../../../config/const/padding.dart';
+import '../../../../../config/const/radius.dart';
+import '../../../../../config/theme/color.dart';
+import '../../../../../config/theme/text_style.dart';
+import '../../../../../gen/assets.gen.dart';
+
+class CreateInvoiceRow extends StatelessWidget {
+  const CreateInvoiceRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    void createSaleInovice() {
+      context.push(AppPath.createSaleInvoice);
+    }
+
+    void createImportInvoice() {
+      context.push(AppPath.createImportInvoice);
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Row(
+        children: [
+          Expanded(
+            child: _createInvoiceButton(
+              onTap: createSaleInovice,
+              color: AppColor.green,
+              title: "Thêm hóa đơn bán",
+              assetGenImage: Assets.icon.invoices,
+              gradient: AppGradient.getGreenGradient(),
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: _createInvoiceButton(
+              onTap: createImportInvoice,
+              title: "Thêm hóa đơn nhập",
+              color: AppColor.blueShade2,
+              assetGenImage: Assets.icon.import,
+              gradient: AppGradient.getBlueGradient(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _createInvoiceButton({
+    required String title,
+    required Gradient gradient,
+    required AssetGenImage assetGenImage,
+    Color? color,
+    final Function()? onTap,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: AppPadding.padding14,
+        decoration: BoxDecoration(
+          gradient: gradient,
+          borderRadius: AppContainerBorder.radius6,
+        ),
+        child: Column(
+          children: [
+            Text(
+              title,
+              style: bodyMedium.copyWith(color: color ?? AppColor.white),
+            ),
+            const SizedBox(height: 7),
+            Image.asset(
+              assetGenImage.keyName,
+              height: 25,
+              width: 25,
+              color: color ?? AppColor.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
