66ed2282ade7ec78d00d2cd45ef0b5669270ddbd
Quang Phạm <phamminhquang.work@gmail.com>
2024-06-11 17:14:50 +0700

    Style UI

---
+++ lib/feature/presentations/widget/chart/chart.dart
@@ -0,0 +1,56 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:mercury/config/const/box_shadow.dart';
+import 'package:mercury/feature/data/model/chart_item/chart_item.dart';
+
+import '../../../../config/const/radius.dart';
+import '../../../../config/theme/color.dart';
+import '../../ui/dashboard/widget/chart_of_day.dart';
+
+class AppChart extends StatelessWidget {
+  const AppChart({super.key, this.maxY, this.items});
+  final double? maxY;
+  final List<ChartItem>? items;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 30),
+      height: 300,
+      decoration: BoxDecoration(
+        color: AppColor.white,
+        boxShadow: defaultBoxShadow,
+        borderRadius: AppContainerBorder.radius6,
+      ),
+      child: BarChart(
+        BarChartData(
+          maxY: maxY,
+          gridData: const FlGridData(
+            show: true,
+            drawVerticalLine: false,
+          ),
+          borderData: FlBorderData(
+            border: Border.all(color: AppColor.grey4, width: 0.7),
+          ),
+          titlesData: const FlTitlesData(
+            show: true,
+            bottomTitles: AxisTitles(),
+            topTitles: AxisTitles(),
+          ),
+          barGroups: items
+              ?.map((e) => BarChartGroupData(
+                    barRods: [
+                      BarChartRodData(
+                          width: 20,
+                          borderRadius: BorderRadius.circular(2),
+                          color: chartColors[items?.indexOf(e) ?? 0],
+                          toY: e.numberSold?.toDouble() ?? 1)
+                    ],
+                    x: items?.indexOf(e) ?? 0,
+                  ))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
